4b944cc2f4fb7ada2c2351379708185900fc0182
Wesley McClure <wes.mcclure@gmail.com>
2014-02-13 17:11:08 -0800

    methods to set journaling globally

---
+++ MongoLearning/SetJournalingGlobally.cs
@@ -0,0 +1,28 @@
+﻿namespace MongoLearning
+{
+	using MongoDB.Driver;
+	using NUnit.Framework;
+
+	public class SetJournalingGlobally : AssertionHelper
+	{
+		[Test]
+		public void ViaCollectionSettings()
+		{
+			var settings = MongoClientSettings.FromUrl(new MongoUrl("mongodb://localhost"));
+			settings.WriteConcern.Journal = true;
+			var collection = new MongoClient(settings)
+				.GetServer().GetDatabase("mydb").GetCollection("mycollection");
+
+			Expect(collection.Settings.WriteConcern.Journal, Is.True);
+		}
+
+		[Test]
+		public void ViaConnectionString()
+		{
+			var collection = new MongoClient("mongodb://localhost/?journal=true")
+				.GetServer().GetDatabase("mydb").GetCollection("mycollection");
+
+			Expect(collection.Settings.WriteConcern.Journal, Is.True);
+		}
+	}
+}
